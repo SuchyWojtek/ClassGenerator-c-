2a3c4a58ea295225b4db0b18b0ce79ffc51f72fa
SuchyWojtek <wojtek.galant97@gmail.com>
2017-05-16 12:19:16 +0200

    Cpp class generator on POB.

---
+++ ClassGenerator/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace ClassGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtClassName = new System.Windows.Forms.TextBox();
+            this.lblClassName = new System.Windows.Forms.Label();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.lblVariable = new System.Windows.Forms.Label();
+            this.lblType = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblVariableName = new System.Windows.Forms.Label();
+            this.btnAddVariable = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtClassName
+            // 
+            this.txtClassName.Location = new System.Drawing.Point(12, 12);
+            this.txtClassName.Name = "txtClassName";
+            this.txtClassName.Size = new System.Drawing.Size(192, 22);
+            this.txtClassName.TabIndex = 0;
+            // 
+            // lblClassName
+            // 
+            this.lblClassName.AutoSize = true;
+            this.lblClassName.Location = new System.Drawing.Point(211, 15);
+            this.lblClassName.Name = "lblClassName";
+            this.lblClassName.Size = new System.Drawing.Size(83, 17);
+            this.lblClassName.TabIndex = 1;
+            this.lblClassName.Text = "Class Name";
+            // 
+            // txtType
+            // 
+            this.txtType.Location = new System.Drawing.Point(12, 75);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(192, 22);
+            this.txtType.TabIndex = 2;
+            // 
+            // lblVariable
+            // 
+            this.lblVariable.AutoSize = true;
+            this.lblVariable.Location = new System.Drawing.Point(9, 46);
+            this.lblVariable.Name = "lblVariable";
+            this.lblVariable.Size = new System.Drawing.Size(98, 17);
+            this.lblVariable.TabIndex = 3;
+            this.lblVariable.Text = "Add variables:";
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Location = new System.Drawing.Point(210, 78);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(40, 17);
+            this.lblType.TabIndex = 4;
+            this.lblType.Text = "Type";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(12, 103);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(192, 22);
+            this.txtName.TabIndex = 5;
+            // 
+            // lblVariableName
+            // 
+            this.lblVariableName.AutoSize = true;
+            this.lblVariableName.Location = new System.Drawing.Point(210, 107);
+            this.lblVariableName.Name = "lblVariableName";
+            this.lblVariableName.Size = new System.Drawing.Size(45, 17);
+            this.lblVariableName.TabIndex = 6;
+            this.lblVariableName.Text = "Name";
+            // 
+            // btnAddVariable
+            // 
+            this.btnAddVariable.Location = new System.Drawing.Point(257, 75);
+            this.btnAddVariable.Name = "btnAddVariable";
+            this.btnAddVariable.Size = new System.Drawing.Size(49, 50);
+            this.btnAddVariable.TabIndex = 7;
+            this.btnAddVariable.Text = "+";
+            this.btnAddVariable.UseVisualStyleBackColor = true;
+            this.btnAddVariable.Click += new System.EventHandler(this.btnAddVariable_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(198, 142);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(108, 30);
+            this.btnGenerate.TabIndex = 8;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(318, 184);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnAddVariable);
+            this.Controls.Add(this.lblVariableName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblType);
+            this.Controls.Add(this.lblVariable);
+            this.Controls.Add(this.txtType);
+            this.Controls.Add(this.lblClassName);
+            this.Controls.Add(this.txtClassName);
+            this.Name = "Form1";
+            this.Text = "Class Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtClassName;
+        private System.Windows.Forms.Label lblClassName;
+        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.Label lblVariable;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblVariableName;
+        private System.Windows.Forms.Button btnAddVariable;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
+
---
+++ ClassGenerator/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ClassGenerator
+{
+    public partial class Form1 : Form
+    {
+        private string[] variablesTypes = new string [20];
+        private int sizeVariablesTypes = 0;
+        private string[] variables = new string [20];
+        private int sizeVariables = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            string className = txtClassName.Text;
+            string folderPath = "";
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "";
+            if (fbd.ShowDialog() == DialogResult.OK)
+                folderPath = fbd.SelectedPath;
+
+            FileStream fileH = new FileStream(folderPath + "\\" + className + ".h", FileMode.Create, FileAccess.Write);
+            FileStream fileCpp = new FileStream(folderPath + "\\" + className + ".cpp", FileMode.Create, FileAccess.Write);
+
+            StreamWriter writeCpp = new StreamWriter(fileCpp);
+            writeCpp.Write( lib() + include() + " \"" + className + ".h\"" + "\n\n" + 
+                className + "::" +  constructor(className) + "\n" + open() + close() + "\n\n    " + 
+                className + "::" + destructor(className) + "\n" + open() + close() + "\n\n" + 
+                className + "::" + className + "(" );
+            // constructor with arg
+            for(int i = 0; i < sizeVariablesTypes; i++)
+            {
+                writeCpp.Write( " " + variablesTypes[i] + " " + variables[i] );
+                if (i + 1 != sizeVariablesTypes)
+                    writeCpp.Write(", ");
+            }
+            writeCpp.Write(" )\n{\n");
+            for(int i = 0; i < sizeVariablesTypes; i++)
+            {
+                writeCpp.Write("    _" + variables[i] + " = " + variables[i] + ";\n");
+            }
+            writeCpp.Write("}\n");
+            for (int i = 0; i < sizeVariablesTypes; i++)
+            {
+                // getter
+                writeCpp.Write("\nconst " + variablesTypes[i] + " " + className + "::get" + 
+                    variables[i].First().ToString().ToUpper() + String.Join("", variables[i].Skip(1))
+                    + "(){\n    return _" + variables[i] + semicolon() + "}\n");
+                // setter
+                writeCpp.Write("\nvoid " + className + "::set" + variables[i].First().ToString().ToUpper() 
+                    + String.Join("", variables[i].Skip(1)) + "(" + variablesTypes[i] + " " + variables[i] +
+                    ")" + open() + "    _" + variables[i] + " = " + variables[i] + ";\n" + close() + "\n");
+            }
+
+            StreamWriter writeH = new StreamWriter(fileH);
+            writeH.Write( pragma() + cname(className) + open() + priv() );
+            for(int i = 0; i < sizeVariablesTypes; i++)
+            {
+                writeH.Write("    " + variablesTypes[i] + " _" + variables[i] + semicolon());
+            }
+            writeH.Write("\n" + publ() + "    " + constructor(className) + semicolon() +
+                "    " + destructor(className) + semicolon() + "    "  + className + "(");
+            // constructor with args
+            for (int i = 0; i < sizeVariablesTypes; i++)
+            {
+                writeH.Write(" " + variablesTypes[i] + " " + variables[i] );
+                if (i + 1 != sizeVariablesTypes)
+                    writeH.Write(", ");
+            }
+            writeH.Write(" );\n");
+            for (int i = 0; i < sizeVariablesTypes; i++)
+            {
+                // getter
+                writeH.Write("    const " + variablesTypes[i] + " get" + variables[i].First().ToString().ToUpper() + 
+                    String.Join("",variables[i].Skip(1)) + "();\n");
+                // setter
+                writeH.Write("    void set" + variables[i].First().ToString().ToUpper() + 
+                    String.Join("", variables[i].Skip(1)) + "(" + variablesTypes[i] + " " + 
+                    variables[i] + ");\n");
+            }
+            writeH.Write("\n" + close() + semicolon());
+            writeH.Close();
+            writeCpp.Close();
+            txtClassName.Clear();
+        }
+
+        private void btnAddVariable_Click(object sender, EventArgs e)
+        {
+            string type = txtType.Text;
+            string name = txtName.Text;
+            variablesTypes[sizeVariablesTypes] = type;
+            variables[sizeVariables] = name;
+            sizeVariables += 1;
+            sizeVariablesTypes += 1;
+            txtType.Clear();
+            txtName.Clear();
+        }
+
+        string lib()
+        {
+            return "#include \"stdafx.h\"\n";
+        }
+
+        string include()
+        {
+            return "#include";
+        }
+
+        string pragma()
+        {
+            return "#pragma once\n\n";
+        }
+
+        string cname(string className)
+        {
+            return "class " + className + "\n";
+        }
+
+        string publ()
+        {
+            return "public:\n";
+        }
+
+        string priv()
+        {
+            return "protected:\n";
+        }
+
+        string constructor(string className)
+        {
+            return className + "()";
+        }
+
+        string destructor(string className)
+        {
+            return "~" + className + "()";
+        }
+
+        string open()
+        {
+            return "{\n";
+        }
+
+        string close()
+        {
+            return "}";
+        }
+
+        string semicolon()
+        {
+            return ";\n";
+        }
+    }
+}
